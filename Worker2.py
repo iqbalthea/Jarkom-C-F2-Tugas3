fbe954c159894d7cbee5d60fe76387646b9d7774
aliirsyaadn <aliirsyaadnursyaban@gmail.com>
2020-10-22 22:33:12 +0700

    Fixing Worker2.py

--- Worker2.py
+++ Worker2.py
@@ -6,7 +6,7 @@ PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+DISCONNECT_MESSAGE = "EXIT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
@@ -14,15 +14,16 @@ server.listen()
 print("[STARTING] server is starting...")
 conn, addr = server.accept()
 
+
 def find_factor(x):
     list_number = []
     x = int(x)
-    print("Faktor dari", x, "adalah:")
     for i in range(1, x+1):
         if x % i == 0:
             list_number.append(i)
-    listToStr = ' '.join(list_number) 
+    listToStr = ' '.join([str(i) for i in list_number])
     return listToStr
+
 
 connected = True
 while connected:
