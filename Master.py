f0106c98e125f201d2393d71ff9cf69f4f664c3c
aliirsyaadn <aliirsyaadnursyaban@gmail.com>
2020-10-22 21:50:12 +0700

    Example socket simple

--- Master.py
+++ Master.py
@@ -1,8 +1,30 @@
 import socket
 
-HEADER = 64
+HEADER = 2048
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+connected = True
+while connected:
+    var = input()
+    send(var)
+    if var == DISCONNECT_MESSAGE:
+        connected = False
+        continue
