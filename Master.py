cbe0474ed88931e685ee9b6d72430e846b4b275b
aliirsyaadn <aliirsyaadnursyaban@gmail.com>
2020-10-25 23:36:40 +0700

    Delete IP in Master.py

--- Master.py
+++ Master.py
@@ -4,8 +4,6 @@ HEADER = 2048
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "EXIT"
 SERVER = socket.gethostbyname(socket.gethostname())
-# SERVER = "3.84.7.20"
-SERVER = "54.237.193.137"
 
 ADDR_1 = (SERVER, 5051)
 ADDR_2 = (SERVER, 5052)
@@ -63,5 +61,3 @@ def send(client, msg):
     else:
         print(f"JOB IS FINISHED")
         print(f"[OUTPUT] = {recv_msg}")
-
-
